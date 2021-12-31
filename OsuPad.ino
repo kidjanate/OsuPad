b4bbe74a55c146f6d0b335cd05031087c6105de8
kidjanate <64061240+kidjanate@users.noreply.github.com>
2021-12-31 22:17:13 +0700

    Fixed bugs and added document

--- OsuPad.ino
+++ OsuPad.ino
@@ -1,13 +1,15 @@
 #include <Keyboard.h>
 
-
+//Bindings
 int buttonPins[] = {21, 3};
 int ledPins[] = {16, 9};
-char buttonOutputs[] = {'f', 'd'};
+char buttonOutputs[] = {'d', 'f'};
 
+//Array Length
 int btnCount = sizeof(buttonPins) / sizeof(buttonPins[0]);
 
 void setup() {
+  //Set pinMode to all keys
   for (int i = 0; i < btnCount; i++){
     pinMode(buttonPins[i], INPUT_PULLUP);
     pinMode(ledPins[i], OUTPUT);
@@ -16,7 +18,9 @@ void setup() {
 }
 
 void loop() {
+  //For loop all keys
   for (int i = 0; i < btnCount; i++){
+    //If button down
     if(digitalRead(buttonPins[i]) == LOW){
       digitalWrite(ledPins[i], HIGH);
       Keyboard.press(buttonOutputs[i]);
